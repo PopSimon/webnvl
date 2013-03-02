3d27444d58ae796fe699b7c10253df77ee1ed231
simon <morzsak@kocsog.hu>
2013-03-02 23:38:25 +0100

    animation + gitignore

--- src/animation.js
+++ src/animation.js
@@ -1,23 +1,86 @@
 "use strict";
 
-var ViewPort = {
-	target: null
-}
-
-var prefixes = {
-	animation: "-webkit-animation"
+var WebkitConsts = {
+	animation: "-webkit-animation",
+    animationEnd: "webkitAnimationEnd"
 };
 
-function animate(/* jQuery element */ element) {
-	var animname = element.data("animation");
-	if (animname) {
-		ViewPort.target.css(prefixes.animation, animname + " 2s");
-	}
+var UserAgentConsts = WebkitConsts;
+
+function AnimationHandler(context) {
+    this.context = context;
+    this.listeners = [];
 }
+AnimationHandler.prototype = Object.create(Object.prototype, {
+    constructor: { value: AnimationHandler },
+    // eg: target1(animationrule1) target2(animationrule2)
+    ANIMEXP: { value: /([^\(\)\s]+)\(([^\(\)]+)\)/g },
+    stopAll: {
+        value: function (/* String selector */ targets) {
+            var targets = this.context.find(targets);
+            targets.css(UserAgentConsts.animation, "");
+            for (var i=0; i < targets.length; ++i) {
+                var target = targets[i];
+                for (var j=0; j < this.listeners.length; ++j) {
+                    var listener = this.listeners[i];
+                    if (listener[0] === target) {
+                        target.removeEventListener(UserAgentConsts.animationEnd, listener[1]);
+                    }
+                    this.listeners.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    },
+    start: {
+        value: function (/* String selector */ targets, /* CSS animation property value */ animrule) {
+            if (targets && animrule) {
+                var onAnimationEnd = (function (e) {
+                    this.stopAll(targets);
+                }).bind(this);
+                
+                targets = this.context.find(targets);
+                targets.css(UserAgentConsts.animation, animrule);
+                for (var i=0; i < targets.length; ++i) {
+                    var target = targets[i];
+                    targets[i].addEventListener(UserAgentConsts.animationEnd, onAnimationEnd, false);
+                    this.listeners.push([target, onAnimationEnd]);
+                }
+            }
+        }
+    },
+    handle: {
+        value: function (/* jQuery element */ element) {
+            var animstr = element.data("animation");
+            if (animstr) {
+                var result;
+                while ((result = this.ANIMEXP.exec(animstr)) !== null) {
+                    var target = result[1];
+                    var animrule = result[2];
+                    this.start(target, animrule);
+                }
+            }
+            
+            var targets = element.data("stop-animations");
+            if (targets) {
+                this.stopAll(targets);
+            }
+        },
+        enumerable: true
+    }
+});
 
 window.onload = function () {
-	ViewPort.target = $("#target");
+    var ah = new AnimationHandler($("body"));
+    
 	setTimeout(function () {
-		animate($("#source"));
-	})
+		ah.handle($("#source1"));
+	});
+    
+    setTimeout(function () {
+		ah.handle($("#source2"));
+	}, 4000);
+    setTimeout(function () {
+		ah.handle($("#source3"));
+	}, 6000);
 }
