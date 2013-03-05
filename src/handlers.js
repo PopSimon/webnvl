a906bfea4fbdce8f55a02226a608bc0164c5fdef
simon <morzsak@kocsog.hu>
2013-03-05 21:45:10 +0100

    asd

--- src/handlers.js
+++ src/handlers.js
@@ -4,66 +4,71 @@
 //    }
 //};
 
-TextHandler = {
-    handle: function (node, viewport) {
-        viewport.text = node.element;
-    }
-};
+var handleScene = (function () {
+    var TextHandler = {
+        handleName: function (element) {
+            var charid = element.attr("char");
+            if (charid) {
+                var character = GAMECONTEXT.characters[charid];
+                GAMECONTEXT.viewport.setSpeaker(character);
+            } else {
+                GAMECONTEXT.viewport.setSpeaker(null);
+            }
+        },
+        handleAvatar: function (element) {
+            var charid = element.attr("char");
+            var avatar = element.attr("avatar");
+            if (charid && avatar) {
+                var character = GAMECONTEXT.characters[charid];
+                GAMECONTEXT.viewport.setAvatar(character, avatar);
+            } else {
+                GAMECONTEXT.viewport.setAvatar(null, null);
+            }
+        },
+        handle: function (element) {
+            GAMECONTEXT.viewport.setText(element.text());
+        }
+    };
 
-AvatarHandler = {
-    handle: function (node, viewport) {
-        var character = node.element.data("character");
-        var mood = node.element.data("mood");
-
-        viewport.avatar = new Avatar(character, mood);
-    }
-};
-
-CharSpritesHandler = {
-    handle: function (node, viewport) {
-        var element = node.characters;
-        
-        if (element.length) {
-            if (element.data("clear")) {
-                viewport.stage.characters.clear();
+    var StageHandler = {
+        handle: function (element) {
+            if (element.attr("clear")) {
+                this.clearCharacterSprites();
             }
             
-            var rtarg = element.data("remove")
-            if (rtarg) { 
-                viewport.stage.characters.remove(rtarg);
-            }
-            viewport.stage.characters.add(element.children("img"));
+            element.find("remove").each((function (index, element) {
+                this.removeCharacterSprite(element);
+            }).bind(this));
+            
+            element.find("add").each((function (index, element) {
+                this.addCharacterSprite(element);
+            }).bind(this));
+        },
+        addCharacterSprite: function (element) {
+            var sprite = new Sprite(
+                element.attr("id"), element.attr("src"), element.attr("class"), 
+                element.attr("left"), element.attr("top"), element.attr("fade"), 
+                element.attr("animation"));
+            GAMECONTEXT.viewport.stage.add(sprite);
+        },
+        removeCharacterSprite: function (element) {
+            GAMECONTEXT.viewport.stage.remove(element.attr("id"), element.attr("fade"));
+        },
+        clearCharacterSprites: function () {
+            GAMECONTEXT.viewport.stage.clear();
         }
+    };
+    
+    function handleBackground(element) {
+        GAMECONTEXT.viewport.setBackground(element.attr("src"), element.attr("fade"));
     }
-};
-
-function HandlerGroup(handlers) {
-    this.handlers = handlers;
-}
-HandlerGroup.prototype = Object.create(Object.prototype, {
-    constructor: { value: HandlerGroup },
-    handle: {
-        value: function (node, viewport) {
-            for (var i = 0; i < this.handlers.length; ++i) {
-                this.handlers[i].handle(node, viewport);
-            }
-        }
+    
+    return function (element) {
+        var bg = element.find("background");
+        if (background) handleBackground(bg);
+        var stage = element.find("stage");
+        if (stage) StageHandler.handle(stage);
+        var text = element.find("text");
+        if (text) StageHandler.handle(text);
     }
-});
-
-var SceneHandler = new HandlerGroup([
-    TextHandler, AvatarHandler
-]);
-
-StageHandler = new HandlerGroup([
-    CharSpritesHandler
-]);
-
-var ChapterHandler = {
-    handle: function (visitor, node) {
-        var ambient = node.element.children("audio.ambient");
-        if (ambient.length) {
-            GAMECONTEXT.viewport.ambient = ambient;
-        }
-    }
-};
+})();
