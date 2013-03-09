4c66af45fd112878ba143f03d63f1e93a0251b69
Pop Simon <morzsak@kocsog.hu>
2013-03-09 15:55:02 +0100

    scenario mod

--- src/scenario.js
+++ src/scenario.js
@@ -20,12 +20,20 @@ function Scenario(/* jQuery element */ element) {
 	this.chapters = {};
     
     this.chapter = null;
-    this.sequence = null;
-    this.screen = null;
+    this.routepart = null;
+    this.__screenIndex__ = 0;
+	this.screen = null;
 }
 Scenario.prototype = {
-    get: function (id) {
-        var ids = id.split(".");
+    splitId: function (id) {
+		if (typeof id == 'string') {
+			return id.split(".");
+		} else {
+			return [id];
+		}
+	},
+	get: function (id) {
+        var ids = this.splitID(id);
         switch (ids.length) {
             case 1:
                 return this.getChapter(ids[0]);
@@ -47,12 +55,53 @@ Scenario.prototype = {
 		}
 		return chapter;
 	},
-	getSequence: function (chapterid, sceneid) {
-		var chapter = this.getChapter(chapterid);
-		return chapter.content[i];
+	getRoutePart: function (id1, id2) {
+		var routepartid;
+		
+		if (!id2) {
+			routepartid = id1;
+			chapter = this.chapter;
+		} else {
+			routepartid = id2;
+			chapter = this.getChapter(id1);
+		}
+		
+		return chapter.content[routepartid];
 	},
-	getScreen: function (chapterid, sceneid, textid) {
-		var text = this.getScene(chapterid, sceneid);
-		return text.content[i];
+	getScreen: function (id1, id2, id3) {
+		var rp = this.getRoutePart(id1, id2);
+		return rp.content[id3];
+	},
+	jump: function (routepartid) {
+		var ids = this.splitID(routepartid);
+        switch (ids.length) {
+            case 1:
+                this.routepart = this.chapter.content[ids[0]];
+				this.__screenIndex__ = 0;
+            break;
+            case 2:				
+				this.chapter = this.getChapter(ids[0]);
+				this.routepart = this.chapter.content[ids[1]];
+				this.__screenIndex__ = 0;
+            break;
+			case 3:
+				this.chapter = this.getChapter(ids[0]);
+				this.routepart = this.chapter.content[ids[1]];
+				this.__screenIndex__ = ids[2];
+			break;
+            default: 
+                throw Error("Bad id!");
+        }
+		
+		this.screen = this.routepart.content[this.__screenIndex__];
+		
+		this.handle(this.screen);
+	},
+	next: function () {
+		if (this.__screenIndex__ < this.routepart.content.length) {
+			this.jump(++this.routepart.content.length);
+		}
+	},
+	handle: function (screen) {
 	}
 };
